a103da15c215d04723bf6ca3888fa5363c30e21b
Masum Billah <masumbillah6584@gmail.com>
2024-06-05 00:53:43 +0600

    project section done

--- components/sections/projects/index.tsx
+++ components/sections/projects/index.tsx
@@ -1,5 +1,11 @@
-import { StickyScroll } from '@/components/framer-motion/sticky-scroll';
+'use client';
+import {
+    CardBody,
+    CardContainer,
+    CardItem,
+} from '@/components/framer-motion/3d-card';
 import Titlebar from '@/components/title-bar';
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 const content = [
     {
@@ -48,12 +54,79 @@ const content = [
             </div>
         ),
     },
+    {
+        title: 'Running out of content',
+        description:
+            "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+        content: (
+            <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+                Running out of content
+            </div>
+        ),
+    },
+    {
+        title: 'Running out of content',
+        description:
+            "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+        content: (
+            <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+                Running out of content
+            </div>
+        ),
+    },
 ];
 const Projects = () => {
     return (
-        <div id="projects" className="relative z-40 min-h-screen">
+        <div id="projects" className="relative z-40 my-10 md:my-14 lg:my-20">
             <Titlebar title="Projects" />
-            
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-10 lg:gap-7 my-4 md:my-6 lg:my-10">
+                {content.map((c) => (
+                    <CardContainer className="inter-var" key={c.title}>
+                        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto h-auto rounded-xl p-6 border  ">
+                            <CardItem
+                                translateZ="50"
+                                className="text-xl font-bold text-neutral-600 dark:text-white">
+                                Make things float in air
+                            </CardItem>
+                            <CardItem
+                                as="p"
+                                translateZ="60"
+                                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
+                                Hover over this card to unleash the power of CSS
+                                perspective
+                            </CardItem>
+                            <CardItem translateZ="100" className="w-full mt-4">
+                                <Image
+                                    src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                    height="1000"
+                                    width="1000"
+                                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                                    alt="thumbnail"
+                                />
+                            </CardItem>
+                            <div className="flex justify-end items-center gap-3 mt-10">
+                                <CardItem
+                                    translateZ={20}
+                                    href="https://twitter.com/mannupaaji"
+                                    target="_blank"
+                                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white">
+                                    Try now →
+                                </CardItem>
+                                <CardItem
+                                    translateZ={20}
+                                    as="button"
+                                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
+                                    Sign up
+                                </CardItem>
+                            </div>
+                        </CardBody>
+                    </CardContainer>
+                ))}
+            </div>
+
+            <div className='w-full flex justify-center items-center'>
+                <Button className='bg-slate-600'>SEE ALL</Button>
+            </div>
         </div>
     );
 };
