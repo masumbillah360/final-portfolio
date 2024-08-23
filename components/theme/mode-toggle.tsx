7c8f4b98aa0dcf9824e8c0aac2f20990c6fe349c
Masum Billah <masumbillah6584@gmail.com>
2024-08-23 15:51:18 +0600

    formatted code

--- components/theme/mode-toggle.tsx
+++ components/theme/mode-toggle.tsx
@@ -23,7 +23,6 @@ export function ModeToggle() {
             ) : (
                 <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             )}
-
             <span className="sr-only">Toggle theme</span>
         </Button>
     );
