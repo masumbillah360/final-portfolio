6aaf96db0d8113a950da98be7523c29fde20fcda
Masum Billah <masumbillah6584@gmail.com>
2025-12-26 00:28:08 +0600

    feat: added function tracer project

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'MERN Stacks | Programming | Web development blogs by Masum Billah',
+    title: 'Fullstack | Programming | Web development blogs by Masum Billah',
     description:
-        'From this page you can see some brand new & latest MERN Stacks | Programming | Web development blogs. Those are made by Masum Billah',
+        'From this page you can see some brand new & latest Fullstack | Programming | Web development blogs. Those are made by Masum Billah',
 };
 const BlogLayout = ({ children }: { children: React.ReactNode }) => {
     return <div className="">{children}</div>;
